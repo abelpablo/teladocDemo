d4b10aba1884f312381fa817ae354d1f167029b5
abelpablo <abelpablo@gmail.com>
2023-10-20 14:33:35 -0300

    Initial commit

---
+++ src/main/java/com/exercice/teladocDemo/TeladocDemoApplication.java
@@ -0,0 +1,13 @@
+package com.exercice.teladocDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeladocDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeladocDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercice/teladocDemo/TeladocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercice.teladocDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeladocDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
